77fbbdbbc8e6079462be2bf8b8d91a2931cbc7f6
nebulabox <nebulabox@gmail.com>
2019-03-13 14:53:40 +0800

    no message

--- Formula/aria2.rb
+++ Formula/aria2.rb
@@ -1,8 +1,6 @@
 class Aria2 < Formula
   desc "Download with resuming and segmented downloading, dig para version, removed some restrictions"
   homepage "https://github.com/nebulabox/aria2"
-  url "https://github.com/nebulabox/aria2/archive/dp.0.1.tar.gz"
-  sha256 "8d9ff87b8e4ff4379cb7675e4b581a56b7bb801b48efc0431b31cc2060ba3ef6"
   head "https://github.com/nebulabox/aria2.git"
 
   depends_on "pkg-config" => :build
@@ -31,8 +29,4 @@ class Aria2 < Formula
     bash_completion.install "doc/bash_completion/aria2c"
   end
 
-  test do
-    system "#{bin}/aria2c", "https://brew.sh/"
-    assert_predicate testpath/"index.html", :exist?, "Failed to create index.html!"
-  end
 end
